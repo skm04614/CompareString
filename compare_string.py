62697f322d5f7f3c28ebff6352b62f10ab30989a
Kevin <skm04614@gmail.com>
2024-06-13 16:06:02 +0900

    feat: implements length score logic

--- compare_string.py
+++ compare_string.py
@@ -1,9 +1,23 @@
 def compare_string(s1: str,
-                   s2: str) -> int:
+                   s2: str) -> float:
     if not s1.isalpha() or not s2.isalpha():
         raise ValueError("Only alphabetical inputs are allowed.")
 
     if not s1.isupper() or not s2.isupper():
         raise ValueError("Only capitalized inputs are allowed.")
 
-    return 0
+    if len(s1) > len(s2):
+        s1, s2 = s2, s1
+
+    a = len(s1)
+    b = len(s2)
+    gap = b - a
+
+    if a == b:
+        return 60.0
+
+    if 2 * a <= b:
+        return 0.0
+
+    return 60.0 * (1 - gap / b)
+
--- test_compare_string.py
+++ test_compare_string.py
@@ -12,3 +12,15 @@ class Test(TestCase):
             for invalid_argument in invalid_arguments:
                 compare_string(valid_argument, invalid_argument)
                 compare_string(invalid_argument, valid_argument)
+
+    def test_length(self):
+        expected_to_argument_pairs = {
+            60.0: (("AB", "CD"), ("EF", "ZO"), ("RC", "BQ")),
+            60.0 * (1 - 2/5): (("AAABB", "CCD"),),
+            60.0 * (1 - 1/3): (("AAB", "CC"),),
+            0.0: (("A", "BCD"), ("RZAB", "QWERTYUIO"), ("ZXC", "ASDFGH")),
+        }
+
+        for expected, argument_pairs in expected_to_argument_pairs.items():
+            for args in argument_pairs:
+                self.assertEqual(expected, compare_string(*args))
