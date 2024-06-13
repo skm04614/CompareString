4825139684ccfbed0fe9faa00acfc415c2d82135
Kevin <skm04614@gmail.com>
2024-06-13 15:52:56 +0900

    feat: implements capitalized alpha check logic

--- compare_string.py
+++ compare_string.py
@@ -1,3 +1,9 @@
 def compare_string(s1: str,
                    s2: str) -> int:
+    if not s1.isalpha() or not s2.isalpha():
+        raise ValueError("Only alphabetical inputs are allowed.")
+
+    if not s1.isupper() or not s2.isupper():
+        raise ValueError("Only capitalized inputs are allowed.")
+
     return 0
--- test_compare_string.py
+++ test_compare_string.py
@@ -1,6 +1,14 @@
 from unittest import TestCase
 
+from compare_string import compare_string
+
 
 class Test(TestCase):
-    def test_compare_string(self):
-        pass
+    def test_invalid_arguments(self):
+        invalid_arguments = ("ab", "a1", "12", "-")
+        valid_argument = "A"
+
+        with self.assertRaises(ValueError):
+            for invalid_argument in invalid_arguments:
+                compare_string(valid_argument, invalid_argument)
+                compare_string(invalid_argument, valid_argument)
