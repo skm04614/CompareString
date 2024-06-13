6bd945f986fce3762a75c29bd1bed9574c6d95d1
Kevin <skm04614@gmail.com>
2024-06-13 16:40:37 +0900

    feat: implements full test logic

--- compare_string.py
+++ compare_string.py
@@ -1,3 +1,28 @@
+def length_comparison_score(s1: str,
+                            s2: str) -> float:
+    a = len(s1)
+    b = len(s2)
+    if a > b:
+        a, b = b, a
+
+    gap = b - a
+    if a == b:
+        return 60.0
+    if 2 * a <= b:
+        return 0.0
+    return 60.0 * (1 - gap / b)
+
+
+def similarity_score(s1: str,
+                     s2: str) -> float:
+    total_cnt = len(set(s1) | set(s2))
+    same_cnt = len(set(s1) & set(s2))
+    if total_cnt == same_cnt:
+        return 40.0
+
+    return 40.0 * same_cnt / total_cnt
+
+
 def compare_string(s1: str,
                    s2: str) -> float:
     if not s1.isalpha() or not s2.isalpha():
@@ -6,18 +31,4 @@ def compare_string(s1: str,
     if not s1.isupper() or not s2.isupper():
         raise ValueError("Only capitalized inputs are allowed.")
 
-    if len(s1) > len(s2):
-        s1, s2 = s2, s1
-
-    a = len(s1)
-    b = len(s2)
-    gap = b - a
-
-    if a == b:
-        return 60.0
-
-    if 2 * a <= b:
-        return 0.0
-
-    return 60.0 * (1 - gap / b)
-
+    return length_comparison_score(s1, s2) + similarity_score(s1, s2)
--- test_compare_string.py
+++ test_compare_string.py
@@ -1,6 +1,6 @@
 from unittest import TestCase
 
-from compare_string import compare_string
+from compare_string import compare_string, length_comparison_score, similarity_score
 
 
 class Test(TestCase):
@@ -19,6 +19,31 @@ class Test(TestCase):
             60.0 * (1 - 2/5): (("AAABB", "CCD"),),
             60.0 * (1 - 1/3): (("AAB", "CC"),),
             0.0: (("A", "BCD"), ("RZAB", "QWERTYUIO"), ("ZXC", "ASDFGH")),
+        }
+
+        for expected, argument_pairs in expected_to_argument_pairs.items():
+            for args in argument_pairs:
+                self.assertEqual(expected, length_comparison_score(*args))
+
+    def test_similarity(self):
+        expected_to_argument_pairs = {
+            0.0: (("AB", "CD"), ("EF", "ZO"), ("RC", "BQ")),
+            40.0: (("A", "A"), ("AB", "BAA"), ("ACAAB", "ABC"), ("XXXXXXY", "YXXXXX")),
+            40.0 * 1/3: (("AAAAA", "ABC"), ("ABC", "C"), ("AABBCCDDEEFF", "ABABABAB")),
+            40.0 * 3/7: (("ABCD", "ABXYZD"), ("AABBCCDDEEFFGG", "EAB"), ("XXXXXYZAAAAA", "XZRTAB"))
+        }
+
+        for expected, argument_pairs in expected_to_argument_pairs.items():
+            for args in argument_pairs:
+                self.assertEqual(expected, similarity_score(*args))
+
+    def test_total_score(self):
+        expected_to_argument_pairs = {
+            100.0: (("AAAAB", "BABBB"), ("A", "A")),
+            60.0: (("AB", "CD"), ("EF", "ZO"), ("RC", "BQ")),
+            40.0 + 60.0 * (1 - 1/7): (("XXXXXXY", "YXXXXX"),),
+            40.0: (("AB", "BAAAAA"), ("ACCCCCCAAB", "ABC")),
+            0.0: (("AAAAB", "C"), ("XYZXYZXXYYZZ", "ASDF"), ("RXY", "M"))
         }
 
         for expected, argument_pairs in expected_to_argument_pairs.items():
